b1d76f7ea4264e18eee6c1c96c424b92a670e717
hbsgujjar111 <hbsgujjar111h@gmail.com>
2022-01-25 16:58:00 +0500

    QR Scanner Added

--- lib/app_screens/online_order.dart
+++ lib/app_screens/online_order.dart
@@ -5,20 +5,15 @@ import 'package:baranh/utils/dynamic_sizes.dart';
 import 'package:baranh/widgets/buttons.dart';
 import 'package:baranh/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:line_icons/line_icon.dart';
-import 'package:line_icons/line_icons.dart';
 import 'package:lottie/lottie.dart';
 
 class OnlineOrder extends StatefulWidget {
   const OnlineOrder({Key? key}) : super(key: key);
-
   @override
   State<OnlineOrder> createState() => _OnlineOrderState();
 }
 
 class _OnlineOrderState extends State<OnlineOrder> {
-  bool _showFab = true;
   dynamic checkState;
   @override
   Widget build(BuildContext context) {
@@ -83,8 +78,8 @@ class _OnlineOrderState extends State<OnlineOrder> {
                                   onTap: () {
                                     showSearch(
                                       context: context,
-                                      delegate:
-                                          CustomSearchDelegate(snapshot.data["menu"]),
+                                      delegate: CustomSearchDelegate(
+                                          snapshot.data["menu"]),
                                     ).then((value) => changeState(() {}));
                                   },
                                   child: Container(
@@ -114,38 +109,17 @@ class _OnlineOrderState extends State<OnlineOrder> {
                                     changeState(() {});
                                   };
                                   return Expanded(
-                                    child: NotificationListener<
-                                            UserScrollNotification>(
-                                        onNotification: (notification) {
-                                          final ScrollDirection direction =
-                                              notification.direction;
-
-                                          if (direction ==
-                                              ScrollDirection.reverse) {
-                                            _showFab = false;
-                                            checkState();
-                                          } else if (direction ==
-                                              ScrollDirection.forward) {
-                                            _showFab = true;
-                                            checkState();
-                                          }
-
-                                          return true;
-                                        },
-                                        child: ListView.builder(
-                                            itemCount:
-                                                snapshot.data["menu"].length,
-                                            itemBuilder: (context, index) {
-                                              return Padding(
-                                                padding: EdgeInsets.symmetric(
-                                                    vertical: dynamicHeight(
-                                                        context, 0.01)),
-                                                child: menuCards(
-                                                    context,
-                                                    snapshot.data["menu"],
-                                                    index),
-                                              );
-                                            })),
+                                    child: ListView.builder(
+                                        itemCount: snapshot.data["menu"].length,
+                                        itemBuilder: (context, index) {
+                                          return Padding(
+                                            padding: EdgeInsets.symmetric(
+                                                vertical: dynamicHeight(
+                                                    context, 0.01)),
+                                            child: menuCards(context,
+                                                snapshot.data["menu"], index),
+                                          );
+                                        }),
                                   );
                                 }),
                               ],
@@ -166,36 +140,6 @@ class _OnlineOrderState extends State<OnlineOrder> {
           ),
         ),
       ),
-      floatingActionButton: StatefulBuilder(builder: (context, stateNotify) {
-        checkState = () {
-          stateNotify(() {});
-        };
-        return AnimatedSlide(
-          duration: const Duration(milliseconds: 300),
-          offset: _showFab ? Offset.zero : const Offset(0, 2),
-          child: AnimatedOpacity(
-            duration: const Duration(milliseconds: 300),
-            opacity: _showFab ? 1 : 0,
-            child: FloatingActionButton.extended(
-              isExtended: true,
-              onPressed: () {},
-              backgroundColor: myOrange,
-              label: Row(
-                children: [
-                  LineIcon(
-                    LineIcons.bell,
-                    color: myWhite,
-                    size: dynamicWidth(context, 0.04),
-                  ),
-                  widthBox(context, 0.01),
-                  text(context, "Call waiter", 0.03, myWhite,
-                      alignText: TextAlign.center, bold: true),
-                ],
-              ),
-            ),
-          ),
-        );
-      }),
     );
   }
 }
--- lib/app_screens/qr_screen.dart
+++ lib/app_screens/qr_screen.dart
@@ -1,10 +1,133 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class QRSreen extends StatelessWidget {
+import 'package:baranh/utils/config.dart';
+import 'package:baranh/utils/dynamic_sizes.dart';
+import 'package:baranh/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRSreen extends StatefulWidget {
   const QRSreen({Key? key}) : super(key: key);
 
   @override
+  State<QRSreen> createState() => _QRSreenState();
+}
+
+class _QRSreenState extends State<QRSreen> {
+  dynamic barcode = "";
+  final qrKey = GlobalKey();
+  QRViewController? controller;
+  void qrCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((event) {
+      setState(() {
+        barcode = event;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    if (Platform.isAndroid) {
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+    super.reassemble();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        QRView(
+          key: qrKey,
+          onQRViewCreated: qrCreated,
+          overlay: QrScannerOverlayShape(
+              borderColor: myOrange,
+              borderLength: 20,
+              borderRadius: 10,
+              borderWidth: 10,
+              cutOutSize: dynamicWidth(context, 0.8)),
+        ),
+        Positioned(
+          bottom: dynamicHeight(context, 0.1),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(dynamicWidth(context, 0.02)),
+            child: Container(
+              color: myWhite.withOpacity(0.4),
+              padding: EdgeInsets.all(dynamicWidth(context, 0.02)),
+              child: text(
+                  context,
+                  // barcode != "" ? "${barcode!.code}" : "Scan the QR code",
+                  "Scan the QR code",
+                  0.04,
+                  myWhite),
+            ),
+          ),
+        ),
+        Positioned(
+            top: dynamicHeight(context, 0.02),
+            child: Container(
+              padding: EdgeInsets.all(dynamicWidth(context, 0.01)),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(dynamicWidth(context, 0.1)),
+                color: Colors.white24,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () async {
+                      await controller?.toggleFlash();
+                      setState(() {});
+                    },
+                    icon: FutureBuilder<bool?>(
+                      future: controller?.getFlashStatus(),
+                      builder: (context, snapshot) {
+                        if (snapshot.data != null) {
+                          return Icon(
+                            snapshot.data! ? Icons.flash_on : Icons.flash_off,
+                            color: myWhite,
+                          );
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      await controller?.flipCamera();
+                      setState(() {});
+                    },
+                    icon: FutureBuilder(
+                      future: controller?.getCameraInfo(),
+                      builder: (context, snapshot) {
+                        if (snapshot.data != null) {
+                          return const Icon(
+                            Icons.switch_camera,
+                            color: myWhite,
+                          );
+                        } else {
+                          return Container();
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ))
+      ],
+    );
   }
 }
